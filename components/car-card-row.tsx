01f3f684ab6d6a53a9132163f0d0a04b4edb824c
wharvex <t.gudlewski@gmail.com>
2023-11-26 21:17:11 -0500

    feat: show car features and prices in "See Cars"
    The "See Cars" page now shows three additional detail rows per card that show the three "car features" that each car has and their prices.
    
    next: merge Tara's and my changes together into test-database branch

--- components/car-card-row.tsx
+++ components/car-card-row.tsx
@@ -1,63 +1,18 @@
 import type { NextPage } from "next";
 import CarCard from "./car-card";
+import { carType } from "@/models/car-type";
 
 type CarCardRowType = {
-  image1?: string;
-  image11?: string;
   hasTwoCards: boolean;
-  makeValueText1: string;
-  modelValueText1: string;
-  colorValueText1: string;
-  yearValueText1: string;
-  typeValueText1: string;
-  priceValueText1: string;
-  makeValueText2: string;
-  modelValueText2: string;
-  colorValueText2: string;
-  yearValueText2: string;
-  typeValueText2: string;
-  priceValueText2: string;
+  car1: carType;
+  car2: carType;
 };
 
-const CarCardRow: NextPage<CarCardRowType> = ({
-  image1,
-  image11,
-  hasTwoCards,
-  makeValueText1,
-  modelValueText1,
-  colorValueText1,
-  yearValueText1,
-  typeValueText1,
-  priceValueText1,
-  makeValueText2,
-  modelValueText2,
-  colorValueText2,
-  yearValueText2,
-  typeValueText2,
-  priceValueText2,
-}) => {
+const CarCardRow: NextPage<CarCardRowType> = ({ hasTwoCards, car1, car2 }) => {
   return (
     <div className="flex flex-row items-start justify-start p-2.5 gap-[111px]">
-      <CarCard
-        image1={image1}
-        makeValueText={makeValueText1}
-        modelValueText={modelValueText1}
-        colorValueText={colorValueText1}
-        yearValueText={yearValueText1}
-        typeValueText={typeValueText1}
-        priceValueText={priceValueText1}
-      />
-      {hasTwoCards && (
-        <CarCard
-          image1={image11}
-          makeValueText={makeValueText2}
-          modelValueText={modelValueText2}
-          colorValueText={colorValueText2}
-          yearValueText={yearValueText2}
-          typeValueText={typeValueText2}
-          priceValueText={priceValueText2}
-        />
-      )}
+      <CarCard car={car1} />
+      {hasTwoCards && <CarCard car={car2} />}
     </div>
   );
 };
--- components/main-content-car-cards.tsx
+++ components/main-content-car-cards.tsx
@@ -1,12 +1,8 @@
 import { useMemo, type CSSProperties } from "react";
 import CarCardRow from "./car-card-row";
-import prisma from "@/lib/prisma";
-import { Prisma } from "@prisma/client";
+import { getCar, getCars, carType, carsType } from "@/models/car-type";
 
 type MainContentCarCardsType = {
-  image1?: string;
-  image11?: string;
-
   /** Style props */
   mainContentCarCardsAlignSelf?: CSSProperties["alignSelf"];
   mainContentCarCardsFlex?: CSSProperties["flex"];
@@ -19,24 +15,6 @@ export default async function MainContentCarCards(fn: MainContentCarCardsType) {
       flex: fn.mainContentCarCardsFlex,
     };
   }, [fn.mainContentCarCardsAlignSelf, fn.mainContentCarCardsFlex]);
-
-  /**
-   * Need to "include" entries from other tables.
-   * Need to wrap this find call in a function to get its return type, based on method found here:
-   * https://www.prisma.io/docs/concepts/components/prisma-client/advanced-type-safety/operating-against-partial-structures-of-model-types#solution-1
-   * */
-  async function getCars() {
-    const cars = await prisma.car.findMany({
-      include: {
-        make: true,
-        model: true,
-        car_type: true,
-      },
-    });
-    return cars;
-  }
-
-  type carsType = Prisma.PromiseReturnType<typeof getCars>;
 
   const cars = await getCars();
 
@@ -58,21 +36,9 @@ export default async function MainContentCarCards(fn: MainContentCarCardsType) {
       {pairwiseCars.map((carPair) => (
         <CarCardRow
           key={carPair[0].id}
-          image1={carPair[0].image_path}
-          image11={carPair[1].image_path}
           hasTwoCards={carPair.length === 2}
-          makeValueText1={carPair[0].make.name}
-          modelValueText1={carPair[0].model.name}
-          colorValueText1={carPair[0].color}
-          yearValueText1={carPair[0].year.toString()}
-          typeValueText1={carPair[0].car_type.name}
-          priceValueText1="ford"
-          makeValueText2="ford"
-          modelValueText2="ford"
-          colorValueText2="ford"
-          yearValueText2="ford"
-          typeValueText2="ford"
-          priceValueText2="ford"
+          car1={carPair[0]}
+          car2={carPair[1]}
         />
       ))}
     </div>
