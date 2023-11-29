c08f2e24a3c0cd8aa03f4e4321b40e0e2712600d
wharvex <t.gudlewski@gmail.com>
2023-11-28 22:03:07 -0500

    feat: refactor getCars call
    The call to getCars and the creation of pairwiseCars have been moved from the MainContentCarCards component to _actions.ts so they can be called together at the page level.

--- components/main-content-car-cards.tsx
+++ components/main-content-car-cards.tsx
@@ -1,11 +1,14 @@
 import { useMemo, type CSSProperties } from "react";
 import CarCardRow from "./car-card-row";
-import { getCar, getCars, carType, carsType } from "@/models/car-type";
+import { PairwiseCars } from "@/app/_actions";
 
 type MainContentCarCardsType = {
   /** Style props */
   mainContentCarCardsAlignSelf?: CSSProperties["alignSelf"];
   mainContentCarCardsFlex?: CSSProperties["flex"];
+
+  /** Content props */
+  pairwiseCars: PairwiseCars;
 };
 
 export default async function MainContentCarCards(fn: MainContentCarCardsType) {
@@ -16,24 +19,12 @@ export default async function MainContentCarCards(fn: MainContentCarCardsType) {
     };
   }, [fn.mainContentCarCardsAlignSelf, fn.mainContentCarCardsFlex]);
 
-  const cars = await getCars();
-
-  /** Adapted this reduce from: https://stackoverflow.com/a/44996257
-   * It splits models into an array of two-item arrays so they can be mapped over 2-by-2.
-   * If models has an odd number of elements, the last array in pairwiseModels will only
-   * have one element.
-   * */
-  const pairwiseCars = cars.reduce<carsType[]>((result, _, index, array) => {
-    if (index % 2 === 0) result.push(array.slice(index, index + 2));
-    return result;
-  }, []);
-
   return (
     <div
       className="[background:linear-gradient(180deg,_#ebf5ff,_#92c9f9)] flex flex-col items-center justify-center py-[50px] px-[100px] gap-[35px]"
       style={mainContentCarCardsStyle}
     >
-      {pairwiseCars.map((carPair) => (
+      {fn.pairwiseCars.map((carPair) => (
         <CarCardRow
           key={carPair[0].id}
           hasTwoCards={carPair.length === 2}
