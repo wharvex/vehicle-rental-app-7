3fa1e706b080c443ed24ac264e09909ad8023568
wharvex <t.gudlewski@gmail.com>
2023-12-16 10:53:49 -0500

    simplify hero components

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,11 @@
-import type { NextPage } from "next";
-import Layout from "../components/my-layout";
 import MainContentGeneric from "@/components/main-content-generic";
 import MainContentHero from "@/components/main-content-hero";
 import MainContentCardsGroup from "@/components/main-content-cards-group";
 
-const Page: NextPage = () => {
-  const onImageHeaderPageLogoClick = () => {};
-
+export default function Page() {
   return (
     <>
-      <MainContentHero
-        mainContentHeroFlexShrink="0"
-        bodyHeroBoxSizing="border-box"
-        bodyHeroBoxSizing1="border-box"
-        bodyHeroBoxSizing2="border-box"
-        bodyHeroBoxSizing3="border-box"
-      />
+      <MainContentHero />
       <MainContentCardsGroup />
       <MainContentGeneric
         text="We Look Forward To Working With You!"
@@ -23,6 +13,4 @@ const Page: NextPage = () => {
       />
     </>
   );
-};
-
-export default Page;
+}
--- components/body-hero.tsx
+++ components/body-hero.tsx
@@ -1,59 +1,9 @@
-import type { NextPage } from "next";
-import { useMemo, type CSSProperties } from "react";
 import ContainerHeroBodyHeader from "./container-hero-body-header";
 import ContainerRadioChoiceMenu from "./container-radio-choice-menu";
 
-type BodyHeroType = {
-  /** Style props */
-  bodyHeroFlexShrink?: CSSProperties["flexShrink"];
-  containerHeroBodyHeaderBoxSizing?: CSSProperties["boxSizing"];
-  containerHeroBodyHeaderBoxSizing1?: CSSProperties["boxSizing"];
-  containerHeroBodyHeaderBoxSizing2?: CSSProperties["boxSizing"];
-  containerHeroBodyHeaderBoxSizing3?: CSSProperties["boxSizing"];
-};
-
-const BodyHero: NextPage<BodyHeroType> = ({
-  bodyHeroFlexShrink,
-  containerHeroBodyHeaderBoxSizing,
-  containerHeroBodyHeaderBoxSizing1,
-  containerHeroBodyHeaderBoxSizing2,
-  containerHeroBodyHeaderBoxSizing3,
-}) => {
-  const bodyHeroStyle: CSSProperties = useMemo(() => {
-    return {
-      flexShrink: bodyHeroFlexShrink,
-    };
-  }, [bodyHeroFlexShrink]);
-
-  const iconRadioChoiceUnselectStyle: CSSProperties = useMemo(() => {
-    return {
-      boxSizing: containerHeroBodyHeaderBoxSizing,
-    };
-  }, [containerHeroBodyHeaderBoxSizing]);
-
-  const iconRadioChoiceUnselectStyle1: CSSProperties = useMemo(() => {
-    return {
-      boxSizing: containerHeroBodyHeaderBoxSizing1,
-    };
-  }, [containerHeroBodyHeaderBoxSizing1]);
-
-  const iconRadioChoiceUnselectStyle2: CSSProperties = useMemo(() => {
-    return {
-      boxSizing: containerHeroBodyHeaderBoxSizing2,
-    };
-  }, [containerHeroBodyHeaderBoxSizing2]);
-
-  const iconRadioChoiceUnselectStyle3: CSSProperties = useMemo(() => {
-    return {
-      boxSizing: containerHeroBodyHeaderBoxSizing3,
-    };
-  }, [containerHeroBodyHeaderBoxSizing3]);
-
+export default function BodyHero() {
   return (
-    <div
-      className="rounded-xl bg-white w-[1000px] h-[350px] overflow-hidden flex flex-row items-center justify-center py-[25px] px-[50px] box-border gap-[75px]"
-      style={bodyHeroStyle}
-    >
+    <div className="rounded-xl [&_*]:shrink-0 bg-white w-[1000px] h-[350px] overflow-hidden flex flex-row items-center justify-center py-[25px] px-[50px] box-border gap-[75px]">
       <ContainerHeroBodyHeader />
       <ContainerRadioChoiceMenu
         containerRadioChoiceMenuBoxSizing="border-box"
@@ -64,6 +14,4 @@ const BodyHero: NextPage<BodyHeroType> = ({
       />
     </div>
   );
-};
-
-export default BodyHero;
+}
