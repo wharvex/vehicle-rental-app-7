22f5c95c0cae4db79a3fb17cb11f876f070003ee
wharvex <t.gudlewski@gmail.com>
2024-02-08 21:55:18 -0500

    remove unique constraint on email field in Manager model

--- app/_actions.ts
+++ app/_actions.ts
@@ -4,20 +4,17 @@ import { getCars, carsType } from "@/models/car-type";
 import { Prisma } from "@prisma/client";
 
 export async function getPairwiseCars() {
-  const cars = await getCars();
+  const cars: carsType[] = await getCars();
 
   /** Adapted this reduce from: https://stackoverflow.com/a/44996257
    * It splits models into an array of two-item arrays so they can be mapped over 2-by-2.
    * If models has an odd number of elements, the last array in pairwiseModels will only
    * have one element.
    * */
-  return cars.reduce<carsType[]>(
-    (result: any[], _: any, index: number, array: string | any[]) => {
-      if (index % 2 === 0) result.push(array.slice(index, index + 2));
-      return result;
-    },
-    []
-  );
+  return cars.reduce<carsType[]>((result, _, index, array) => {
+    if (index % 2 === 0) result.push(array.slice(index, index + 2));
+    return result;
+  }, []);
 }
 
 export type PairwiseCars = Prisma.PromiseReturnType<typeof getPairwiseCars>;
--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,19 +1,19 @@
 /**
  * To avoid exhausting the database connections by creating multiple Prisma Client instances,
  * use this Prisma Singleton pattern, as described here:
- * 
+ *
  * https://www.prisma.io/docs/guides/other/troubleshooting-orm/help-articles/nextjs-prisma-client-dev-practices
  */
 import { PrismaClient as PrismaClient1 } from "@prisma-db-1/client";
 import { PrismaClient as PrismaClient2 } from "@prisma-db-2/client";
 
-let PrismaClient: typeof PrismaClient1 | typeof PrismaClient2;
-if (!process.env.NODE_ENV || process.env.NODE_ENV === 'development') {
-    // dev code
-    PrismaClient = PrismaClient2;
+let PrismaClient: any;
+if (!process.env.NODE_ENV || process.env.NODE_ENV === "development") {
+  // dev code
+  PrismaClient = PrismaClient2;
 } else {
-    // production code
-    PrismaClient = PrismaClient1;
+  // production code
+  PrismaClient = PrismaClient1;
 }
 const prismaClientSingleton = () => {
   return new PrismaClient();
